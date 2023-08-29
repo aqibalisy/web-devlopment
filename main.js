8dc2a38ff93ba557dbe52a26b0721b54d5c649bb
Aqib Ali <101803091+aqibalisy@users.noreply.github.com>
2023-08-30 03:23:54 +0530

    fibonacciGenerator

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-let arr = [];
-let count = 0;
 
-function fizzBuzz() {
-    if (count % 3 === 0 && count % 5 === 0) {
-        arr.push("FizzBuzz")
-    } else if (count % 3 === 0) {
-        arr.push("Fizz")
-    } else if (count % 5 == 0) {
-        arr.push("Buzz")
-    } else {
-        arr.push(count);
+// let x = 0;
+// let y = 1;
+// let z = x+y
+// console.log(x);
+// console.log(z);
+// while(z<10){
+//     console.log(z)
+//     x=y;
+//     y=z;
+//     z=x+y;
+    
+// } 
+
+function fibonacciGenerator (n) {
+    
+    //Write your code here:
+    let output = [];
+    if (n===1){
+        output=[0];
     }
-
-    console.log(arr)
-
-    count++;
-
+    else if (n===2){
+        output=[0,1];
+    }
+    else{
+        output=[0,1]
+        for (i=2; i<n; i++){
+            output.push(output[output.length-2] + output[output.length-1])
+            
+        }
+    }
+    return output;
+    
 }
+let a = fibonacciGenerator(5)
+console.log(a)
+
