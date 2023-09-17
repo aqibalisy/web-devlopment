6a5f350927026bd2babcfc5106932406104999c0
Aqib Ali <101803091+aqibalisy@users.noreply.github.com>
2023-09-18 03:34:28 +0530

    learning usage of npm modules

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+// let generateName = require('sillyname');
+// import generateName from 'sillyname'
+// let sillyName = generateName();
+// console.log(`My name is ${sillyName}`);
+
+import superheroes from 'superheroes'
+// superheroes.all;
+let hero = superheroes.random();
+console.log(`My hero Name is ${hero}`);
